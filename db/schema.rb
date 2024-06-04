5aca98de57c66ed15067422ca36500b22553f7c2
Valentin  MICHEL <164916642+ValentinMich349@users.noreply.github.com>
2024-06-04 16:53:41 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -41,14 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_074508) do
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+  
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
