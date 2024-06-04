767dcb6e6f8e266672bd8e0a3aecbda8093895a2
michel349u <michel349u@etu.univ-lorraine.fr>
2024-06-04 15:55:37 +0200

    navbar dropdown add + routes+ gemfile + jquery(js) +application_controller add pseudo to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+     # permet d accepter le pseudo pour devise
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:pseudo])
+    end
+
+   
 end
