b39aa7cf2e06da230459e61fe4b5b57cb0ddc53b
michel349u <michel349u@etu.univ-lorraine.fr>
2024-06-11 09:15:19 +0200

    home page update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
  private
 
   def product_params
-    params.require(:product).permit(:title, :content)
+    params.require(:product).permit(:name, :content)
   end
 
   def set_product
